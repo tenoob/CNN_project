c4cf67ce37c164cc37c3d3bf70e77e24efbc723c
tenoob <18bcs6218@gmail.com>
2022-12-15 23:47:20 +0530

    added create_direc

--- src/cnnProject/utils/common.py
+++ src/cnnProject/utils/common.py
@@ -33,6 +33,21 @@ def read_yaml(file_path:Path) -> ConfigBox:
         raise e
 
     @ensure_annotations
+    def create_directories(path_to_directories:list,verbose=True):
+        """
+        Create list of directories
+
+        Args:
+            path_to_directories (list): list of path of directories
+            ignore_log(bool,optional): ignore if multiple dirs is to be created. Defaults to false
+        """
+        for path in path_to_directories:
+            os.makedirs(path,exist_ok=True)
+            if verbose:
+                logger.info(f"created directory at: {path}")
+
+
+    @ensure_annotations
     def save_json(path:Path , data: dict):
         """
         Save json data
