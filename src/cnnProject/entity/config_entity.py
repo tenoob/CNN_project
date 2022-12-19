4bb340ca8081ff8040fef29f2ae84b13fc9ff124
tenoob <18bcs6218@gmail.com>
2022-12-20 01:15:46 +0530

    data ingestion entity added

---
+++ src/cnnProject/entity/config_entity.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+#this will act similar namedtuple
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
