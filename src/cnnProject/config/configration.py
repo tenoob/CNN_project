698500d896a9cb6874f65fb200132be95ad7e5e9
tenoob <18bcs6218@gmail.com>
2023-01-24 00:32:14 +0530

    added training configration module

--- src/cnnProject/config/configration.py
+++ src/cnnProject/config/configration.py
@@ -1,7 +1,12 @@
 from cnnProject.constant import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from cnnProject.utils import read_yaml, create_directories
 import os
-from cnnProject.entity import DataIngestionConfig , BaseModelPreparationConfig,CallbackPreparationConfig
+from cnnProject.entity import (
+    DataIngestionConfig,
+    BaseModelPreparationConfig,
+    CallbackPreparationConfig,
+    TrainingConfig,
+)
 from pathlib import Path
 
 
@@ -26,38 +31,56 @@ class ConfigrationManger:
         )
         return data_ingestion_config
 
-
-    def get_base_model_perparation_config(self) -> BaseModelPreparationConfig :
+    def get_base_model_perparation_config(self) -> BaseModelPreparationConfig:
         config = self.config.base_model_preparation
 
         create_directories([config.root_dir])
 
         base_model_prepare_config = BaseModelPreparationConfig(
-            root_dir = Path(config.root_dir),
-            base_model_path= Path(config.base_model_path),
-            updated_base_model_path= Path(config.updated_base_model_path),
-            params_image_size = self.params.IMAGE_SIZE,
-            params_learning_rate = self.params.LEARNING_RATE,
-            params_include_top = self.params.INCLUDE_TOP,
-            params_weights = self.params.WEIGHTS,
-            params_classes=self.params.CLASSES
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES,
         )
 
         return base_model_prepare_config
 
-    def get_callback_perparation_config(self) -> CallbackPreparationConfig :
+    def get_callback_perparation_config(self) -> CallbackPreparationConfig:
         config = self.config.callback_preparation
 
         model_checkpoint_dir = os.path.dirname(config.checkpoint_model_filepath)
-        create_directories([
-            Path(config.tensorboard_root_log_dir),
-            Path(model_checkpoint_dir)
-        ])
+        create_directories(
+            [Path(config.tensorboard_root_log_dir), Path(model_checkpoint_dir)]
+        )
 
         callback_prepare_config = CallbackPreparationConfig(
-            root_dir = Path(config.root_dir),
-            tensorboard_root_log_dir = Path(config.tensorboard_root_log_dir),
-            checkpoint_model_filepath = Path(config.checkpoint_model_filepath)
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
         )
 
         return callback_prepare_config
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.base_model_preparation
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "PetImages")
+        create_directories([Path(training.root_dir)])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTAION,
+            params_image_size=params.IMAGE_SIZE,
+        )
+
+        return training_config
