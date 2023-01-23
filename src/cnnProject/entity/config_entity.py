9dca3e9d34524468959dc2fd34e78a295f616901
tenoob <18bcs6218@gmail.com>
2023-01-24 00:31:12 +0530

    added training dataclass

--- src/cnnProject/entity/config_entity.py
+++ src/cnnProject/entity/config_entity.py
@@ -15,7 +15,7 @@ class DataIngestionConfig:
 class BaseModelPreparationConfig:
     root_dir: Path
     base_model_path: Path
-    updated_base_model_path: Path 
+    updated_base_model_path: Path
     params_image_size: list
     params_learning_rate: float
     params_include_top: bool
@@ -28,3 +28,15 @@ class CallbackPreparationConfig:
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: Path
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path  # base untrained vgg16 archtecture model
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
